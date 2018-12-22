7994a845af151e09475d9dc609bdd943368c0ae8
octoduchilli <joada@octo.com>
2018-12-22 19:44:05 +0100

    feat (main-top-bar/search-input): link-to search

--- app/pods/components/main-top-bar/search-input/component.js
+++ app/pods/components/main-top-bar/search-input/component.js
@@ -1,8 +1,13 @@
-import Component from '@ember/component';
+import Component from '@ember/component'
+import { inject as service } from '@ember/service'
 
 export default Component.extend({
+  user: service('current-user'),
+
   tagName: 'div',
   classNames: 'work',
+
+  searchType: null,
 
   click () {
     if (this.totalResults === undefined) {
