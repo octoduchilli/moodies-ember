ef6593015942da823e47aa4132cba78c35d1a5e6
octoduchilli <joada@octo.com>
2018-12-26 00:23:30 +0100

    feat: create tmdb-person-images

---
+++ app/adapters/tmdb-person-images.js
@@ -0,0 +1,21 @@
+import tmdb from './tmdb';
+
+const defaultQuery = {
+  api_key: '3836694fa8a7ae3ea69b5ff360b3be0b',
+  language: 'fr-FR',
+  include_image_language: 'fr,en'
+}
+
+export default tmdb.extend({
+  pathForType() {
+    return 'person'
+  },
+
+  findRecord (store, type, id, snapshot) {
+    let url = this.buildURL(type.modelName, id, snapshot, 'findRecord');
+
+    url += '/images'
+
+    return this.ajax(url, 'GET', { data: defaultQuery });
+  }
+});
