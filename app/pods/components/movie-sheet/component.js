0a86b2876d3ab64228eef0f42c8b8017387726e6
octoduchilli <joada@octo.com>
2018-12-25 21:37:24 +0100

    feat (movie-sheet): new style

--- app/pods/components/movie-sheet/component.js
+++ app/pods/components/movie-sheet/component.js
@@ -1,19 +1,30 @@
-import Component from '@ember/component';
-import preloadImg from 'moodies-ember/mixins/preload-tmdb-img';
-import { inject as service } from '@ember/service';
-import { task, timeout, all } from 'ember-concurrency';
-import { computed } from '@ember/object';
-import { set } from '@ember/object'
+import Component from '@ember/component'
+import preloadImg from 'moodies-ember/mixins/preload-tmdb-img'
+import lerpColor from 'moodies-ember/mixins/lerp-color'
+import { task, all, timeout } from 'ember-concurrency'
+import { inject as service } from '@ember/service'
+import { computed } from '@ember/object'
+import { htmlSafe } from '@ember/string'
+import { set, get } from '@ember/object'
 
-export default Component.extend(preloadImg, {
+export default Component.extend(preloadImg, lerpColor, {
   tagName: 'div',
 
+  firebaseApp: service(),
   progress: service('page-progress'),
+  session: service(),
+  notify: service('notification-messages'),
   store: service(),
   user: service('current-user'),
 
+  id: null,
   movie: null,
-  isShowingPoster: false,
+  images: null,
+  voteAverage: null,
+  newVoteAverage: null,
+
+  sliceCast: 5,
+  sliceCrew: 5,
 
   runtime: computed('movie.runtime', function () {
     if (this.movie) {
@@ -28,12 +39,26 @@ export default Component.extend(preloadImg, {
     return '0 h 00'
   }),
 
-  didReceiveAttrs() {
-    set(this, 'movie', null)
-
-    if (this.movieId) {
-      this.__fetchData.perform(this.movieId);
+  slicedCast: computed('movie.credits.cast', 'sliceCast', function () {
+    if (this.movie.credits && this.movie.credits.cast) {
+      return this.movie.credits.cast.slice(0, this.sliceCast)
     }
+  }),
+
+  slicedCrew: computed('movie.credits.crew', 'sliceCrew', function () {
+    if (this.movie.credits && this.movie.credits.crew) {
+      return this.movie.credits.crew.slice(0, this.sliceCrew)
+    }
+  }),
+
+  async init () {
+    this._super(...arguments)
+
+    this.fetch.perform(this.id)
+  },
+
+  didUpdateAttrs () {
+    this.fetch.perform(this.id)
   },
 
   willDestroyElement () {
@@ -41,48 +66,67 @@ export default Component.extend(preloadImg, {
   },
 
   actions: {
-    togglePoster () {
-      this.toggleProperty('isShowingPoster')
+    voteAverageBorderColorStyle (average) {
+      if (average !== null) {
+        return htmlSafe(`border: 2px solid ${this.lerpColor({r: 255, g: 0, b: 0}, {r: 0, g: 255, b: 0}, average / 10)}`)
+      }
     },
-    scrollTop () {
-      window.scroll({
-        top: 0
+    updateVoteAverage () {
+      set(this, 'newVoteAverage', Math.round((event.offsetX / event.target.offsetWidth) * 10))
+    },
+    async saveVote (average) {
+      let payload = {
+        average: average
+      }
+
+      if (!this.voteAverage) {
+        payload.createdAt = new Date().toString()
+      } else {
+        payload.modifiedAt = new Date().toString()
+      }
+
+      await this.firebaseApp.database().ref(`users/${get(this.session, 'uid')}/vote/${this.movie.id}`).update(payload)
+
+      set(this, 'voteAverage', average)
+    },
+    async deleteVote () {
+      await this.firebaseApp.database().ref(`users/${get(this.session, 'uid')}/vote/${this.movie.id}`).set(null)
+
+      set(this, 'voteAverage', null)
+    },
+    notifyNotConnected () {
+      this.notify.info(`Vous n'êtes pas connecté...`)
+    },
+    showMorePerson (array, slice, sliceKey) {
+      if (array.length > slice) {
+        set(this, sliceKey, slice + 10)
+      }
+    },
+    showAllPerson (array, sliceKey) {
+      set(this, sliceKey, array.length)
+    },
+    resetPerson (sliceKey, className) {
+      set(this, sliceKey, 5)
+
+      setTimeout(() => {
+        window.scroll({
+          top: document.getElementsByClassName(className)[0].offsetTop - 100,
+          behavior: 'smooth'
+        })
       })
     }
   },
 
-  __fetchData: task(function* (movieId) {
+  fetch: task(function*(id) {
     this.progress.reset()
+    this.progress.update(20)
 
     yield all([
       timeout(500),
-      this.__fetchMovie(movieId)
+      this.__fetch.perform(id)
     ])
 
-    return this.progress.update(100)
-  }).restartable(),
-
-  async __fetchMovie (movieId) {
-    await this.store.find('tmdb-movie', movieId).then(_ => {
-      set(this, 'movie', _)
-    })
-
     const movie = this.movie
-
-    this.user.addActivity({
-      id: movie.id,
-      name: movie.title,
-      icon: 'search',
-      type: 'movie'
-    })
-
-    if (movie.belongs_to_collection) {
-      await this.store.find('tmdb-collection', movie.belongs_to_collection.id).then(collection => {
-        set(movie, 'collection', collection)
-      })
-    }
-
-    // TODO set(movie, 'credits.cast', serialized) not set before __fetchData have finish
 
     if (movie.credits) {
       if (movie.credits.cast) {
@@ -100,10 +144,51 @@ export default Component.extend(preloadImg, {
       }
     }
 
-    await this.preloadTMDBImg(this.__getPaths(movie), true)
+    yield this.preloadTMDBImg(this.__getPaths(movie))
 
-    return
-  },
+    this.user.addActivity({
+      id: this.movie.id,
+      name: this.movie.title,
+      icon: 'search',
+      type: 'movie'
+    })
+
+    this.progress.update(100)
+  }).restartable(),
+
+  __fetch: task(function*(id) {
+    yield this.fetchMovie.perform(id)
+    yield this.fetchImages.perform(id)
+    yield this.fetchVote.perform(id)
+  }),
+
+  fetchMovie: task(function*(id) {
+    return yield this.store.find('tmdb-movie', id).then(async movie => {
+      if (movie.belongs_to_collection) {
+        await this.store.find('tmdb-collection', movie.belongs_to_collection.id).then(collection => set(movie, 'collection', collection))
+      }
+
+      this.progress.update(40)
+
+      set(this, 'movie', movie)
+    })
+  }),
+
+  fetchImages: task(function*(id) {
+    return yield this.store.find('tmdb-movie-images', id).then(images => {
+      this.progress.update(60)
+
+      set(this, 'images', images)
+    })
+  }),
+
+  fetchVote: task(function*(id) {
+    return yield this.store.find('fb-user-vote', id).then(vote => {
+      this.progress.update(80)
+
+      set(this, 'voteAverage', vote.average)
+    })
+  }),
 
   __getPaths (movie) {
     let paths = []
@@ -118,7 +203,7 @@ export default Component.extend(preloadImg, {
 
     if (movie.credits) {
       if (movie.credits.cast) {
-        movie.credits.cast.forEach(person => {
+        movie.credits.cast.slice(0, 5).forEach(person => {
           if (person.profile_path) {
             paths.push(person.profile_path)
           }
@@ -126,7 +211,7 @@ export default Component.extend(preloadImg, {
       }
 
       if (movie.credits.crew) {
-        movie.credits.crew.forEach(person => {
+        movie.credits.crew.slice(0, 5).forEach(person => {
           if (person.profile_path) {
             paths.push(person.profile_path)
           }
@@ -134,7 +219,7 @@ export default Component.extend(preloadImg, {
       }
     }
 
-    movie.recommendations.results.forEach(result => {
+    movie.recommendations.results.slice(0, 5).forEach(result => {
       paths.push(result.poster_path)
     })
 
