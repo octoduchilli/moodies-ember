2e247c03765c4759463b054b4cb31bdb19393f5c
octoduchilli <joada@octo.com>
2018-12-26 01:20:48 +0100

    feat: init videos route

--- app/pods/videos/route.js
+++ app/pods/videos/route.js
@@ -1,4 +1,19 @@
-import Route from '@ember/routing/route';
+import Route from '@ember/routing/route'
+import { inject as service } from '@ember/service'
 
 export default Route.extend({
+  user: service('current-user'),
+
+  async model ({ id }) {
+    return await this.store.find('tmdb-movie', id).then(movie => {
+      this.user.addActivity({
+        id: id,
+        name: movie.title,
+        icon: 'youtube',
+        type: 'videos'
+      })
+
+      return { videos: movie.videos.results}
+    })
+  }
 });
