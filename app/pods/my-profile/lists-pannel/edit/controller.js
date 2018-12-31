ba133867e63ba8c94f51ab5a256886dc0952201b
octoduchilli <joada@octo.com>
2018-12-31 10:51:21 +0100

    feat: create lists-pannel route

---
+++ app/pods/my-profile/lists-pannel/edit/controller.js
@@ -0,0 +1,114 @@
+import Controller from '@ember/controller'
+import { inject as service } from '@ember/service'
+import { task, timeout, all } from 'ember-concurrency'
+import { set, computed } from '@ember/object'
+import { copy } from '@ember/object/internals'
+
+export default Controller.extend({
+  router: service (),
+  notify: service('notification-messages'),
+  media: service(),
+  user: service('current-user'),
+
+  list: null,
+
+  props: null,
+  propsToCheck: null,
+
+  errorLabel: '',
+  errorName: '',
+
+  willDeleteList: false,
+  deleted: false,
+
+  haveChanges: computed('propsToCheck', 'props.color', 'props.name', 'props.label', 'props.position', function () {
+    if (this.props) {
+      return Object.entries(this.propsToCheck).every(values => String(this.props[values[0]]) === String(values[1])) === false
+    }
+  }),
+
+  save: task(function*() {
+    if (this.haveChanges) {
+
+      if (!this.props.label) {
+        return set(this, 'errorLabel', 'Ne peut pas être vite')
+      } else if (!this.props.name) {
+        return set(this, 'errorName', 'Ne peut pas être vite')
+      } else if (this.props.position < 1) {
+        set(this.props, 'position', 1)
+      } else if (this.props.position > this.user.lists.length) {
+        set(this.props, 'position', this.user.lists.length)
+      }
+
+      yield all([
+        timeout(750),
+        this.__save()
+      ])
+
+      this.notify.success('Les modifications ont bien été sauvegardées')
+    }
+  }),
+
+  async __save () {
+    const payload = Object.assign({}, this.props, {
+      modifiedAt: new Date().toString()
+    })
+
+    set(payload, 'position', Number(payload.position))
+
+    if (Number(payload.position) !== Number(this.propsToCheck)) {
+      const list = this.user.lists.findBy('position', Number(payload.position))
+
+      set(list, 'position', Number(this.propsToCheck.position))
+
+      await list.save()
+    }
+
+    for (const i in payload) {
+      set(this.list, i, payload[i])
+    }
+
+    await this.list.save()
+
+    set(this, 'propsToCheck', copy(this.props))
+  },
+
+  delete: task(function*() {
+    yield all([
+      timeout(750),
+      this.__delete()
+    ])
+
+    set(this, 'deleted', true)
+    set(this, 'willDeleteList', false)
+
+    this.notify.success('Votre liste a bien été supprimé')
+  }),
+
+  async __delete () {
+    await this.list.destroyRecord()
+  },
+
+  fetchList: task(function*(id) {
+    while (this.user.fetch.isRunning) {
+      yield timeout(300)
+    }
+
+    const list = this.user.lists.findBy('id', id)
+
+    if (!list) {
+      return this.router.transitionTo('my-profile.lists-pannel.new')
+    }
+
+    const obj = {
+      label: list.label,
+      name: list.name,
+      color: list.color,
+      position: list.position
+    }
+
+    set(this, 'list', list)
+    set(this, 'props', copy(obj))
+    set(this, 'propsToCheck', copy(obj))
+  })
+})
---
+++ app/pods/my-profile/lists-pannel/new/controller.js
@@ -0,0 +1,52 @@
+import Controller from '@ember/controller'
+import { inject as service } from '@ember/service'
+import { task, timeout, all } from 'ember-concurrency'
+import { set } from '@ember/object'
+
+export default Controller.extend({
+  notify: service('notification-messages'),
+  media: service(),
+  user: service('current-user'),
+
+  created: false,
+
+  label: null,
+  name: null,
+  color: '#551A8B',
+
+  errorLabel: '',
+  errorName: '',
+
+  save: task(function*() {
+    if (!this.label) {
+      return set(this, 'errorLabel', 'Ne peut pas être vite')
+    } else if (!this.name) {
+      return set(this, 'errorName', 'Ne peut pas être vite')
+    }
+
+    yield all([
+      timeout(750),
+      this.__save()
+    ])
+
+    set(this, 'created', true)
+
+    set(this, 'label', null)
+    set(this, 'name', null)
+    set(this, 'color', '#551A8B')
+
+    this.notify.success('Votre nouvelle liste a été créé avec succès')
+  }),
+
+  async __save () {
+    const payload = {
+      color: this.color,
+      createdAt: new Date().toString(),
+      label: this.label,
+      name: this.name,
+      position: this.user.lists ? this.user.lists.length + 1 : 1
+    }
+
+    await this.store.createRecord('fb-user-lists', payload).save()
+  }
+})
