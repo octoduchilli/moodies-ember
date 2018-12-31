64e77ea758773b8662f85c3fdcce645c88e36677
octoduchilli <joada@octo.com>
2018-12-31 11:00:56 +0100

    fixs

--- app/pods/my-profile/lists-pannel/route.js
+++ app/pods/my-profile/lists-pannel/route.js
@@ -1,5 +1,9 @@
 import Route from '@ember/routing/route'
 
 export default Route.extend({
-
+  redirect(model, transition) {
+    if (transition.targetName === 'my-profile.lists-pannel.index'){
+      return this.transitionTo('my-profile.lists-pannel.new')
+    }
+  }
 })
