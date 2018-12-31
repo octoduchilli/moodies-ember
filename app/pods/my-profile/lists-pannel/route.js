7c937d5a9e8c54fd9a1251d641d208f40977baca
octoduchilli <joada@octo.com>
2018-12-31 13:38:45 +0100

    window scroll

--- app/pods/my-profile/lists-pannel/route.js
+++ app/pods/my-profile/lists-pannel/route.js
@@ -2,6 +2,9 @@ import Route from '@ember/routing/route'
 
 export default Route.extend({
   redirect(model, transition) {
+    window.scroll({
+      top: 0
+    })
     if (transition.targetName === 'my-profile.lists-pannel.index'){
       return this.transitionTo('my-profile.lists-pannel.new')
     }
