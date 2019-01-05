fe64ba8a8129f50101e8595d7d5c1a88ed3f5aa1
octoduchilli <joada@octo.com>
2019-01-05 02:19:46 +0100

    feat: init community route

--- app/pods/community/controller.js
+++ app/pods/community/controller.js
@@ -1,5 +1,12 @@
 import Controller from '@ember/controller'
+import { htmlSafe } from '@ember/string'
 
 export default Controller.extend({
+  activityList: null,
 
+  actions: {
+    updateUserProfileImgPos (x, y, scale) {
+      return htmlSafe(`transform: translate(calc(-50% + ${x * (50 / 150)}px), calc(-50% + ${y * (50 / 150)}px)) scale(${scale})`)
+    }
+  }
 })
--- app/pods/community/route.js
+++ app/pods/community/route.js
@@ -1,6 +1,61 @@
 import Route from '@ember/routing/route'
 import RouteHistoryMixin from 'ember-route-history/mixins/routes/route-history'
+import { set } from '@ember/object'
 
 export default Route.extend(RouteHistoryMixin, {
+  model () {
+    return this.store.findAll('fb-community-last').then(lasts => {
+      let model = {}
 
+      lasts.forEach(last => set(model, last.id, last))
+
+      return model
+    })
+  },
+
+  setupController (controller, model) {
+    this._super(controller, model)
+
+    controller.set('activityList', [])
+
+    controller.get('activityList').pushObject(Object.assign(
+      {},
+      {
+        title: 'Dernier ajout',
+        infos: [
+          {
+            message: 'Dans sa liste',
+            value: model.add.value
+          }
+        ],
+        createdAt: model.add.createdAt,
+        movie: model.add.movie,
+        user: model.add.user
+      }
+    ))
+    controller.get('activityList').pushObject(Object.assign(
+      {},
+      {
+        title: 'Dernier favori',
+        createdAt: model.favorite.createdAt,
+        movie: model.favorite.movie,
+        user: model.favorite.user
+      }
+    ))
+    controller.get('activityList').pushObject(Object.assign(
+      {},
+      {
+        title: 'Dernier vote',
+        infos: [
+          {
+            message: 'A noté',
+            value: `${model.vote.value} / 10`
+          }
+        ],
+        createdAt: model.vote.createdAt,
+        movie: model.vote.movie,
+        user: model.vote.user
+      }
+    ))
+  }
 });
