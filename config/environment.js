65c14b80c9682301834f9cad47abc873f106fbbc
octoduchilli <joada@octo.com>
2019-01-16 11:19:26 +0100

    moodies firebase

--- config/environment.js
+++ config/environment.js
@@ -23,13 +23,22 @@ module.exports = function(environment) {
       // when it is created
     },
 
-    firebase: {
-      apiKey: 'AIzaSyCoEI1RieubL2z_TIwD4R5FHokpgGMvngs',
-      authDomain: 'app-film-bd1b7.firebaseapp.com',
-      databaseURL: 'https://app-film-bd1b7.firebaseio.com',
-      projectId: 'app-film-bd1b7',
-      storageBucket: 'app-film-bd1b7.appspot.com',
-      messagingSenderId: '796383962142'
+    // firebase: {
+    //   apiKey: 'AIzaSyCoEI1RieubL2z_TIwD4R5FHokpgGMvngs',
+    //   authDomain: 'app-film-bd1b7.firebaseapp.com',
+    //   databaseURL: 'https://app-film-bd1b7.firebaseio.com',
+    //   projectId: 'app-film-bd1b7',
+    //   storageBucket: 'app-film-bd1b7.appspot.com',
+    //   messagingSenderId: '796383962142'
+    // },
+
+    firebase : {
+      apiKey: 'AIzaSyDNZiZQTmoc3yd0qaOrGqjvvK9zpv9FebA',
+      authDomain: 'moodies-fr.firebaseapp.com',
+      databaseURL: 'https://moodies-fr.firebaseio.com',
+      projectId: 'moodies-fr',
+      storageBucket: 'moodies-fr.appspot.com',
+      messagingSenderId: '297594988637'
     },
 
     torii: {
